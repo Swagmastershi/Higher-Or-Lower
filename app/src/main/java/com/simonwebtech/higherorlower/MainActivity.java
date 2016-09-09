e278a415fe00ccd890ecb771eddbd18999af9c0f
Simon Shi <simonshi1997@gmail.com>
2016-09-09 09:50:58 -0400

    Change layout of home page

--- app/src/main/java/com/simonwebtech/higherorlower/MainActivity.java
+++ app/src/main/java/com/simonwebtech/higherorlower/MainActivity.java
@@ -2,9 +2,14 @@ package com.simonwebtech.higherorlower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void checkGuess(View view) {
+
+        System.out.println("Button tapped!");
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
